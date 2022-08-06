96465d86a71e47684874539463fbdde0f50327c5
mdsaadk <mohammedsaadk.in@gmail.com>
2022-08-06 11:14:58 +0530

    committing composer

--- javascript.js
+++ javascript.js
@@ -1,40 +1,7 @@
 //scroll to top functionality
 const arrowUp= document.querySelector("#arrow-up");
 
-//arrow to top functionality
-// arrowUp.addEventListener("click",() => {
-//     window.arrowTo({
-//        top:0,
-//        left:0,
-//        behavior:"smooth",
-//     });
-// });
-
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
-
-
-// Nav resume-menu selections
-const resume = document.querySelector("#resume-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
-
-//resume menu function
-// resume.addEventListener("click",() =>{
-//     ul.classlist.toggle("show");
-// });
-
-//close resume menu when a link is clicked
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
-
 // post call
-// var name=document.getElementById('name').value;
-//   var email=document.getElementById('email').value;
-//   var msg=document.getElementById('message').value;
 function submitForm(){
   var name=document.getElementById('name').value;
   var email=document.getElementById('email').value;
@@ -47,7 +14,6 @@ function submitForm(){
     
     xhr.send(JSON.stringify(obj));
     xhr.onload = function() {
-    // window.location.reload();
+    
     }
-
 }
